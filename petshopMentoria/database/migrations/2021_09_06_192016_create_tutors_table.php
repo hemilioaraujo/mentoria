f780f51b97562a8b5d36e93c9871489911450f3f
hemilioaraujo <hemilioaraujo@gmail.com>
2021-09-06 16:41:43 -0300

    add model e migration de Tutor

---
+++ petshopMentoria/database/migrations/2021_09_06_192016_create_tutors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTutorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tutores', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nome',30);
+            $table->string('telefone', 15);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tutores');
+    }
+}
