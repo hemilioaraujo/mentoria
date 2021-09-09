a43e70320ba90183ff09ed0cd77feee58582aad4
hemilioaraujo <hemilioaraujo@gmail.com>
2021-09-09 17:56:32 -0300

    Add regras de validação ao Model

--- petshopMentoria/app/Models/Animal.php
+++ petshopMentoria/app/Models/Animal.php
@@ -12,4 +12,24 @@ class Animal extends Model
     protected $fillable = ['nome', 'idade', 'tipo', 'raca'];
     // identificador único, nome, idade, se é gato ou cachorro e sua respectiva raça; 
     // Além do nome e telefone para contato de seu dono.
+
+    public static function rules()
+    {
+        return [
+            'nome' => ['required', 'max:25'],
+            'idade' => ['required', 'integer'],
+            'tipo' => ['required', 'in:gato,cachorro'],
+            'raca' => ['required', 'max:15'],
+        ];
+    }
+
+    public static function messages()
+    {
+        return [
+            'required' => 'O campo de :attribute é obrigatório.',
+            'integer' => 'O campo de :attribute deve ser do tipo inteiro.',
+            'max' => 'O campo de :attribute não pode ser maior que :max.',
+            'in' => 'O campo de :attribute deve ser um dos tipos: :values'
+        ];
+    }
 }
--- petshopMentoria/app/Models/Tutor.php
+++ petshopMentoria/app/Models/Tutor.php
@@ -8,6 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Tutor extends Model
 {
     use HasFactory;
-    protected $table='tutores';
+    protected $table = 'tutores';
     protected $fillable = ['nome', 'telefone'];
+
+    public static function rules()
+    {
+        return [
+            'nome' => ['required', 'max:30'],
+            'telefone' => ['required', 'max:15', 'regex:/(\(\d{2}\))(\d{4,5}\-\d{4})/i'],
+            'cpf'=>['required']
+        ];
+    }
+
+    public static function messages()
+    {
+        return [
+            'required' => 'O campo de :attribute é obrigatório.',
+            'max' => 'O campo de :attribute não pode ser maior que :max.',
+            'regex' => 'O :attribute é invalido. (xx)xxxxx-xxxx ou (xx)xxxx-xxxx.'
+        ];
+    }
 }
