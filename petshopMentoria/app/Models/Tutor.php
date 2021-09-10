f0ac4389d11db216f3c598b1d91c8343210f9e27
hemilioaraujo <hemilioaraujo@gmail.com>
2021-09-09 22:19:19 -0300

    add atributo cpf

--- petshopMentoria/app/Models/Tutor.php
+++ petshopMentoria/app/Models/Tutor.php
@@ -16,7 +16,7 @@ class Tutor extends Model
         return [
             'nome' => ['required', 'max:30'],
             'telefone' => ['required', 'max:15', 'regex:/(\(\d{2}\))(\d{4,5}\-\d{4})/i'],
-            'cpf'=>['required']
+            'cpf'=>['required','unique:tutores']
         ];
     }
 
@@ -25,7 +25,13 @@ class Tutor extends Model
         return [
             'required' => 'O campo de :attribute é obrigatório.',
             'max' => 'O campo de :attribute não pode ser maior que :max.',
-            'regex' => 'O :attribute é invalido. (xx)xxxxx-xxxx ou (xx)xxxx-xxxx.'
+            'regex' => 'O :attribute é invalido. (xx)xxxxx-xxxx ou (xx)xxxx-xxxx.',
+            'unique'=> 'O :attribute informado já está cadastrado.'
         ];
+    }
+
+    public function animais()
+    {
+        return $this->hasMany(Animal::class);
     }
 }
