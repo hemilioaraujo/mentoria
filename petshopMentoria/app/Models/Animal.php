92d7b0a4ea32a462816e5a1a8132ad3d04481045
hemilioaraujo <hemilioaraujo@gmail.com>
2021-09-09 22:19:06 -0300

    add atributo tutor_id

--- petshopMentoria/app/Models/Animal.php
+++ petshopMentoria/app/Models/Animal.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Animal extends Model
 {
     use HasFactory;
     protected $table = 'animais';
-    protected $fillable = ['nome', 'idade', 'tipo', 'raca'];
-    // identificador único, nome, idade, se é gato ou cachorro e sua respectiva raça; 
+    protected $fillable = ['nome', 'idade', 'tipo', 'raca', 'tutor_id'];
+    // identificador único, nome, idade, se é gato ou cachorro e sua respectiva raça;
     // Além do nome e telefone para contato de seu dono.
 
     public static function rules()
@@ -20,6 +21,7 @@ class Animal extends Model
             'idade' => ['required', 'integer'],
             'tipo' => ['required', 'in:gato,cachorro'],
             'raca' => ['required', 'max:15'],
+            'tutor_id' => ['required', 'exists:tutores,id']
         ];
     }
 
@@ -29,7 +31,13 @@ class Animal extends Model
             'required' => 'O campo de :attribute é obrigatório.',
             'integer' => 'O campo de :attribute deve ser do tipo inteiro.',
             'max' => 'O campo de :attribute não pode ser maior que :max.',
-            'in' => 'O campo de :attribute deve ser um dos tipos: :values'
+            'in' => 'O campo de :attribute deve ser um dos tipos: :values',
+            'exists'=> 'O id de tutor não existe.'
         ];
+    }
+
+    public function tutor()
+    {
+        return $this->belongsTo(Tutor::class);
     }
 }
