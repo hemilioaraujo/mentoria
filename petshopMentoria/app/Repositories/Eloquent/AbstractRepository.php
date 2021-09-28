6e6d7d06a3409493e8d7502b40edf54775e45684
hemilioaraujo <hemilioaraujo@gmail.com>
2021-09-28 18:48:00 -0300

    add crud methods

--- petshopMentoria/app/Repositories/Eloquent/AbstractRepository.php
+++ petshopMentoria/app/Repositories/Eloquent/AbstractRepository.php
@@ -2,6 +2,8 @@
 
 namespace App\Repositories\Eloquent;
 
+use Illuminate\Http\Request;
+
 abstract class AbstractRepository
 {
     protected $model;
@@ -19,5 +21,25 @@ abstract class AbstractRepository
     public function all()
     {
         return $this->model->all();
+    }
+
+    public function find(int $id)
+    {
+        return $this->model->find($id);
+    }
+
+    public function update(array $data, int $id)
+    {
+        return $this->model->whereId($id)->update($data);
+    }
+
+    public function delete(int $id)
+    {
+        return $this->model->destroy($id);
+    }
+    
+    public function create(array $data)
+    {
+        return $this->model->create($data);
     }
 }
