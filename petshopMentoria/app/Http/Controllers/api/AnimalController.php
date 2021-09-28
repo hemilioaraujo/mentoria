00fbf7443a1f93298c97977189acd83ff9338409
hemilioaraujo <hemilioaraujo@gmail.com>
2021-09-28 18:47:13 -0300

    add Interface repository

--- petshopMentoria/app/Http/Controllers/api/AnimalController.php
+++ petshopMentoria/app/Http/Controllers/api/AnimalController.php
@@ -2,8 +2,8 @@
 
 /**
  * [TODO:]
- * Criar requests para cada verbo HTTP
  * Estudar services e repositores
+ * 
  */
 
 namespace App\Http\Controllers\api;
@@ -19,12 +19,6 @@ use Illuminate\Http\Response;
 
 class AnimalController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     * Método GET
-     */
     public function index(AnimalRepositoryInterface $model)
     {
         $animais = $model->all();
@@ -34,49 +28,26 @@ class AnimalController extends Controller
         return Response($animais, StatusCodeInterface::STATUS_OK);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function post(AnimalRequest $request)
+    public function post(AnimalRepositoryInterface $model, AnimalRequest $request)
     {
         $request->validate(Animal::rules(), Animal::messages());
-        $animal = Animal::create($request->all());
-        // $animal = Animal::create($request->all());
+        $animal = $model->create($request->all());
         return Response($animal, StatusCodeInterface::STATUS_CREATED);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function show(AnimalRepositoryInterface $model, int $id)
     {
-        $animal = Animal::with('tutor')->findOrFail($id);
-        // try {
-        // } catch (\Throwable $th) {
-        //     // dd($th);
-        //     return Response($th->getMessage(), StatusCodeInterface::HTTP_OK);
-        // }
+        $animal = $model->find($id);
+
         if ($animal) {
             return Response($animal, StatusCodeInterface::STATUS_OK);
         }
+        return Response([], StatusCodeInterface::STATUS_NOT_FOUND);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param  int                      $id
-     * @return \Illuminate\Http\Response
-     */
-    public function patch(Request $request, $id)
+    public function patch(AnimalRepositoryInterface $model, Request $request, int $id)
     {
-        if (Animal::whereId($id)->update($request->all())) {
+        if ($model->update($request->all(), $id)) {
             return Response(
                 ['status' => 'Recurso atualizado com sucesso.'],
                 StatusCodeInterface::STATUS_OK
@@ -84,15 +55,15 @@ class AnimalController extends Controller
         }
 
         return Response(
-            ['status' => 'Recurso não encontrado.'],
+            [],
             StatusCodeInterface::STATUS_NOT_FOUND
         );
     }
 
-    public function put(Request $request, $id)
+    public function put(AnimalRepositoryInterface $model, Request $request, int $id)
     {
         $request->validate(Animal::rules(), Animal::messages());
-        if (Animal::whereId($id)->update($request->all())) {
+        if ($model->update($request->all(), $id)) {
             return Response(
                 ['status' => 'Recurso atualizado com sucesso.'],
                 StatusCodeInterface::STATUS_OK
@@ -100,20 +71,14 @@ class AnimalController extends Controller
         }
 
         return Response(
-            ['status' => 'Recurso não encontrado.'],
+            [],
             StatusCodeInterface::STATUS_NOT_FOUND
         );
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function delete($id)
+    public function delete(AnimalRepositoryInterface $model, int $id)
     {
-        if (Animal::destroy($id)) {
+        if ($model->delete($id)) {
             return Response(
                 [],
                 StatusCodeInterface::STATUS_OK
--- petshopMentoria/app/Repositories/Contracts/AnimalRepositoryInterface.php
+++ petshopMentoria/app/Repositories/Contracts/AnimalRepositoryInterface.php
@@ -2,7 +2,12 @@
 
 namespace App\Repositories\Contracts;
 
+use Illuminate\Http\Request;
 interface AnimalRepositoryInterface
 {
     public function all();
+    public function find(int $id);
+    public function update(array $data, int $id);
+    public function delete(int $id);
+    public function create(array $data);
 }
