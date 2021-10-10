b0180a071c592441641359ce00f06cb91b604a8c
hemilioaraujo <hemilioaraujo@gmail.com>
2021-10-10 14:05:48 -0300

    add FuncionarioServico

---
+++ petshopMentoria/app/Models/FuncionarioServico.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FuncionarioServico extends Model
+{
+    use HasFactory;
+
+    protected $table = 'funcionarios_servicos';
+}
