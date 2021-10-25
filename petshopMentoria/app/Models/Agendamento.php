ae33ae06467d5a015424513b4d412eb6505558dd
hemilioaraujo <hemilioaraujo@gmail.com>
2021-10-25 09:49:30 -0300

    add relacionamento para animal, funcionário e serviço

--- petshopMentoria/app/Models/Agendamento.php
+++ petshopMentoria/app/Models/Agendamento.php
@@ -10,4 +10,19 @@ class Agendamento extends Model
     use HasFactory;
 
     protected $fillable = ['animal_id', 'funcionario_id', 'data_hora', 'servico_id'];
+
+    public function animal()
+    {
+        return $this->belongsTo(Animal::class);
+    }
+
+    public function funcionario()
+    {
+        return $this->belongsTo(Funcionario::class);
+    }
+
+    public function servico()
+    {
+        return $this->belongsTo(Servico::class);
+    }
 }
