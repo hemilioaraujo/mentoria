549342199737cb3bff8d903b01d583596c850441
hemilioaraujo <hemilioaraujo@gmail.com>
2021-10-25 09:48:03 -0300

    add relacionamento com Agendamento

--- petshopMentoria/app/Models/Funcionario.php
+++ petshopMentoria/app/Models/Funcionario.php
@@ -15,4 +15,25 @@ class Funcionario extends Model
     {
         return $this->belongsToMany(Servico::class, 'funcionarios_servicos');
     }
+    
+    public function agendamentos()
+    {
+        return $this->hasMany(Agendamento::class);
+    }
+
+    /**
+     * Verifica se o funcionário realiza o tipo de serviço
+     *
+     * @param integer $servico_id
+     * @return boolean
+     */
+    public function fazServico(int $servico_id)
+    {
+        $servicos = $this->servicos();
+        if ($servicos->contains($servico_id)) {
+            return true;
+        }
+        return false;
+    }
+
 }
