3c54f765aeb1fcb4bffcad6e443d4664be6f1014
hemilioaraujo <hemilioaraujo@gmail.com>
2021-11-04 17:43:29 -0300

    criado TutorResource

---
+++ petshopMentoria/app/Http/Resources/TutorResource.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TutorResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'nome' => $this->nome,
+            'telefone' => $this->telefone,
+            'cpf' => $this->cpf,
+            'animais' => $this->animaisToArray(),
+        ];
+    }
+
+    public function animaisToArray()
+    {
+        $colecao = [];
+        $count = 0;
+
+        foreach ($this->animais as $animal) {
+            $count += 1;
+            $colecao[] = [
+                'id' => $animal->id,
+                'nome' => $animal->nome,
+                'idade' => $animal->idade,
+                'tipo' => $animal->tipo,
+            ];
+        }
+        return $colecao;
+    }
+}
