b9cb9805595a03397b1811d1e4aa63cceac977e7
hemilioaraujo <hemilioaraujo@gmail.com>
2022-01-07 14:54:34 -0300

    configuração de conexão do redis para api

--- petshopMentoria/config/database.php
+++ petshopMentoria/config/database.php
@@ -141,7 +141,14 @@ return [
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
-
+        
+        'api' => [
+            // 'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_DB', '3'),
+        ],
     ],
 
 ];
